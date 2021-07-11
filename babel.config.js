b2999571b61d104c2ccbda77ca8ce8dd86d21f4e
Alexandre carvalho marques <alexandreacm.marques@gmail.com>
2021-07-11 17:24:49 -0300

    feat: added all libraries at project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    'babel-plugin-styled-components',
+    'inline-dotenv',
+    [
+      'module-resolver',
+      {
+        cwd: ['packagejson'],
+        root: ['.'],
+        extensions: ['.js', '.json', 'jsx', '.ios.js', '.android.js'],
+        alias: {
+          '@': './src/',
+        },
+      },
+    ],
+  ],
 };
